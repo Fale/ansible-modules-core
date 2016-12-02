bc1a55abbf7a351ec5cdebddbe8ac9481eb43b0d
Fabio Alessandro Locati <fale@redhat.com>
2016-12-02 13:03:04 +0000

    Native YAML - files/file.py

--- files/file.py
+++ files/file.py
@@ -100,12 +100,14 @@ EXAMPLES = '''
     group: foo
     state: link
 - file:
-    src: /tmp/{{ item.src }}
-    dest: "{{ item.dest }}"
+    src: '/tmp/{{ item.src }}'
+    dest: '{{ item.dest }}'
     state: link
   with_items:
-    - { src: 'x', dest: 'y' }
-    - { src: 'z', dest: 'k' }
+    - src: x
+      dest: y
+    - src: z
+      dest: k
 
 # touch a file, using symbolic modes to set the permissions (equivalent to 0644)
 - file:
@@ -124,7 +126,6 @@ EXAMPLES = '''
     path: /etc/some_directory
     state: directory
     mode: 0755
-
 '''
 
 import errno
